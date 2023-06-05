9f3fb1008cf9f213d51cb0b473c1960a1730136d
Bobjasu <utsab12.bcet@gmail.com>
2023-06-05 15:41:50 +0530

    1st commit

---
+++ src/test/java/practice/SampleTest.java
@@ -0,0 +1,36 @@
+package practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SampleTest {
+	@Test(groups="SmokeTest")
+	public void m1() {
+		System.out.println("m1 is running");
+		WebDriver driver ;
+		String BROWSER=System.getProperty("browser");
+		String URL=System.getProperty("url");
+		
+		switch (BROWSER) {
+		case "chrome":
+			WebDriverManager.chromedriver().setup();
+            driver=new ChromeDriver();			
+			break;
+			
+		case "firefox":
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+
+		default:
+			driver=new EdgeDriver();
+			break;
+		}		
+		driver.get(URL);
+	}
+
+}
